4f96620006a11684a706fd1e1065b06a0c89a648
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2024-11-19 13:28:14 +0100

    put in my own data/relations

--- scripts/Yvette_PiecewiseSEM.R
+++ scripts/Yvette_PiecewiseSEM.R
@@ -4,7 +4,7 @@ rm(list = ls())
 library(piecewiseSEM)
 
 # read the pointdata
-pointdata_init<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSgbmMwgqYtc_Vac7djGpaQucleD4ZsqyiDMDFHrboEbxqJi4W1LvyJDclFl-WlPxcu7x3HQ8guG2NF/pub?gid=0&single=true&output=csv")
+pointdata_init<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTZ5ObFosqVNTOMG6P87CCcqE4oiXYkuSQixNGR6qex1E4TbvT4wDsn6w5zC-hOVIOQRifzsbZXxuss/pub?gid=964721627&single=true&output=csv")
 pointdata <- pointdata_init |> # Remove rows with missing values
   na.omit() |>   # keep complete cases
   dplyr:: filter(woody>0, woody<20)   # remove 2 extreme values and avoid interpolated negative values
@@ -15,7 +15,7 @@ pointdata <- pointdata_init |> # Remove rows with missing values
 sum(is.na(pointdata))
 colSums(is.na(pointdata))
 
-
+  
 psych::pairs.panels(pointdata,stars = T, ellipses = F)
 
 
@@ -24,8 +24,8 @@ psych::pairs.panels(pointdata,stars = T, ellipses = F)
 # browseURL("https://docs.google.com/presentation/d/1PB8rhbswyPew-FYULsw1pIl8Jyb1FFElKPf34DZrEY8/edit?usp=sharing")
 
 # Model 1: woody predicted by burnfreq and rainfall
-model_woody <- lm(woody ~  cec +burnfreq, 
-                  data = pointdata)
+model_woody <- lm(woody ~  dist2river + burnfreq, 
+             data = pointdata)
 summary(model_woody)
 p1<-ggplot(data=pointdata,aes(x=burnfreq,y=woody))+
   geom_point() +
@@ -33,30 +33,34 @@ p1<-ggplot(data=pointdata,aes(x=burnfreq,y=woody))+
               formula= y~x,
               se=T)
 p1
-p2<-ggplot(data=pointdata,aes(x=cec,y=woody))+
+
+p2<-ggplot(data=pointdata,aes(x=dist2river,y=woody))+
   geom_point() +
   geom_smooth(method="lm",
-              #              method.args=list(family=Gamma(link="log")),
+#              method.args=list(family=Gamma(link="log")),
               formula= y~x,
               se=T) 
 p2
 
 # Model_burnfreq: burning frequency predicted by Core Protected Areas and Rainfall
-model_burnfreq_init <- glm(burnfreq ~ CorProtAr + rainfall, 
-                           family=poisson, 
-                           data = pointdata)
+model_burnfreq_init <- glm(burnfreq ~ dist2river + rainfall, 
+              family=poisson, 
+              data = pointdata)
 # Calculate dispersion statistic
 dispersion_stat <- summary(model_burnfreq_init)$deviance / summary(model_burnfreq_init)$df.residual
 dispersion_stat
 # If 𝜙≈1 : No evidence of overdispersion → Poisson is appropriate. (mean≈variance)
 # If 𝜙>1 : Overdispersion is present → Consider quasi-Poisson or negative binomial.
 # If 𝜙<1 : Underdispersion (less common) → Investigate the data further.
+
+### there is overdispersion
+
 library(MASS)
-model_burnfreq <- MASS::glm.nb(burnfreq ~ CorProtAr + rainfall, 
-                               data = pointdata)
+model_burnfreq <- MASS::glm.nb(burnfreq ~ dist2river + rainfall, 
+              data = pointdata)
 summary(model_burnfreq)
 
-p3<-ggplot(data=pointdata,aes(y=burnfreq,x=CorProtAr))+
+p3<-ggplot(data=pointdata,aes(y=burnfreq,x=dist2river))+
   geom_jitter(width = 0.05, height = 0.1) +
   geom_smooth(method="glm",
               method.args=list(family=quasipoisson),  # close to glm.nb
@@ -71,46 +75,40 @@ p4<-ggplot(data=pointdata,aes(y=burnfreq,x=rainfall))+
               se=T)
 p4
 
-# model_cec: predicted by rainfall
-
-model_cec <- glm(cec ~ rainfall + CorProtAr, 
-                 data = pointdata)
-summary(model_cec)
-
-p5<-ggplot(data=pointdata,aes(y=cec,x=rainfall))+
-  geom_point() +
-  geom_smooth(method="lm",
+# model_dist2river:  predicted by elevation
+model_dist2river <-glm(dist2river~rainfall + elevation,
+                      family=quasipoisson,
+                      data=pointdata)
+summary(model_dist2river)
+p5<-ggplot(data=pointdata,aes(y=dist2river,x=elevation))+
+  geom_jitter(height = 0.02) +
+  geom_smooth(method="glm",
+              method.args=list(family=quasipoisson),
               formula= y~x,
               se=T)
 p5
 
-p6<-ggplot(data=pointdata,aes(y=cec,x=CorProtAr))+
-  geom_point() +
-  geom_smooth(method="lm",
+p6<-ggplot(data=pointdata,aes(y=dist2river,x=rainfall))+
+  geom_jitter(width = 0.05, height = 0.1) +
+  geom_smooth(method="glm",
+              method.args=list(family=quasipoisson),
               formula= y~x,
               se=T)
 p6
 
+# model_rainfall: rainfall predicted by elevation
+model_rainfall <- glm(rainfall ~ elevation+hills, 
+              data = pointdata)
+summary(model_rainfall)
 
-# model_CorProtAra:  predicted by elevation
-model_CorProtAr <-glm(CorProtAr~elevation,
-                      family=binomial,
-                      data=pointdata)
-summary(model_CorProtAr)
-p7<-ggplot(data=pointdata,aes(y=CorProtAr,x=elevation))+
-  geom_jitter(height = 0.02) +
-  geom_smooth(method="glm",
-              method.args=list(family=binomial),
+p7<-ggplot(data=pointdata,aes(y=rainfall,x=elevation))+
+  geom_point() +
+  geom_smooth(method="lm",
               formula= y~x,
               se=T)
 p7
 
-# model_rainfall: rainfall predicted by elevation
-model_rainfall <- glm(rainfall ~ elevation, 
-                      data = pointdata)
-summary(model_rainfall)
-
-p8<-ggplot(data=pointdata,aes(y=rainfall,x=elevation))+
+p8<-ggplot(data=pointdata,aes(y=rainfall,x=hills))+
   geom_point() +
   geom_smooth(method="lm",
               formula= y~x,
@@ -119,23 +117,24 @@ p8
 
 # combine the figures
 library(patchwork)
-allplots<-p1+p2+p3+p4+p5+p6+p7+
+allplots<-p1+p2+p3+p4+p5+p6+p7+p8+
   patchwork::plot_layout(ncol=3) +
-  patchwork::plot_annotation(title="Relations in model 1")
+  patchwork::plot_annotation(title="Relations in the model")
 allplots
 
 ####### Combine all models into a single piecewise SEM
 psem_model <- piecewiseSEM::psem(model_woody,
                                  model_burnfreq,
-                                 model_cec,
-                                 model_CorProtAr,
+                                 model_dist2river,
                                  model_rainfall)
 
 # Summarize the SEM results
 summary(psem_model)
 
+summary(SEMdatastd)
+any(is.na(SEMdatastd))  # Check for missing values
 
-
+          
 # a Significant (P<0.05) global goodness of fit means that your model does not fit well, 
 # indicating potential problems like missing paths, mis-specfied relations, 
 # or unaccounted-for correlations
@@ -153,5 +152,5 @@ summary(psem_model)
 # - Avoid Overfitting:
 #   Add paths only when justified by theory or strong evidence, not purely to improve fit.
 # Common pitfall: 
-# - ignofing significant d-separation tests and failing to modify the model
+# - ignoring significant d-separation tests and failing to modify the model
 # - adding too many variables and pathways, leading to overfitting and loss of parsimony
