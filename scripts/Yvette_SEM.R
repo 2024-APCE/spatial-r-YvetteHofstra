82805b2cb9d0c98bcba6b2ae49a66fcf0e11987b
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2024-11-20 10:40:22 +0100

    added hills~~elevation as they are correlated.

--- scripts/Yvette_SEM.R
+++ scripts/Yvette_SEM.R
@@ -38,15 +38,12 @@ multreg_std<-lm(woody~burnfreq+dist2river+cec+
                 rainfall+elevation+hills+CorProtAr,data=SEMdatastd)
 summary(multreg_std)
 
-# visualization of the result: 
-# browseURL("https://docs.google.com/presentation/d/1Q7uXC5Wiu0G4Xsp5uszCNHKOnf1IMI9doY-13Wbay4A/edit?usp=sharing")
-
 # Make a lavaan model as hypothesized in the Anderson et al 2007 paper and fit the model 
 Woody_model<-"woody ~ burnfreq + dist2river + rainfall + elevation + hills
   burnfreq ~ dist2river + rainfall
   dist2river ~ rainfall + elevation
   rainfall ~ elevation + hills
-  hills ~ elevation
+  hills ~~ elevation
 "
 Woody_model
 Woody_fit<-lavaan::sem(Woody_model,data=SEMdatastd)
@@ -57,13 +54,15 @@ summary(Woody_fit,standardized=T,fit.measures=T,rsquare=T)
 # goodness of fit (should be >0.9): CFI and TLI
 # badness of fit: ( should be <0.1): RMSEA, SRMR
 
+# All are correct except RMSEA, but everything I have tried will make it worse
+
 # visualize the model
 library(lavaanPlot)
 
 lavaanPlot(model = Woody_fit, 
            coefs = TRUE,          # Display coefficients
            stand = TRUE,          # Use standardized coefficients
-           graph_options = list(rankdir = "LR"),  # Top-to-bottom layout
+           graph_options = list(rankdir = "LR"),  # left to right layout
            stars = "regress")     # Add stars for significant paths
 
 
