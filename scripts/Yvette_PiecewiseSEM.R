dbba75c265b988c7c27fd555e008b7e25b61e561
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2024-11-20 10:12:38 +0100

    added the rest of the model relations I mentioned in the SEM script

--- scripts/Yvette_PiecewiseSEM.R
+++ scripts/Yvette_PiecewiseSEM.R
@@ -2,9 +2,10 @@
 renv::restore()
 rm(list = ls())
 library(piecewiseSEM)
+library(ggplot2)
 
 # read the pointdata
-pointdata_init<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTZ5ObFosqVNTOMG6P87CCcqE4oiXYkuSQixNGR6qex1E4TbvT4wDsn6w5zC-hOVIOQRifzsbZXxuss/pub?gid=964721627&single=true&output=csv")
+pointdata_init<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTZ5ObFosqVNTOMG6P87CCcqE4oiXYkuSQixNGR6qex1E4TbvT4wDsn6w5zC-hOVIOQRifzsbZXxuss/pub?gid=964721627&single=true&output=csv")
 pointdata <- pointdata_init |> # Remove rows with missing values
   na.omit() |>   # keep complete cases
   dplyr:: filter(woody>0, woody<20)   # remove 2 extreme values and avoid interpolated negative values
@@ -20,11 +21,8 @@ psych::pairs.panels(pointdata,stars = T, ellipses = F)
 
 
 # Define the models
-# I started from this initially hypothesized causal scheme, my model 1)
-# browseURL("https://docs.google.com/presentation/d/1PB8rhbswyPew-FYULsw1pIl8Jyb1FFElKPf34DZrEY8/edit?usp=sharing")
-
-# Model 1: woody predicted by burnfreq and rainfall
-model_woody <- lm(woody ~  dist2river + burnfreq, 
+# Model 1: woody predicted by burnfreq, rainfall, dist2river, elevation and hills
+model_woody <- lm(woody ~  burnfreq + dist2river + rainfall + elevation + hills, 
              data = pointdata)
 summary(model_woody)
 p1<-ggplot(data=pointdata,aes(x=burnfreq,y=woody))+
@@ -42,7 +40,32 @@ p2<-ggplot(data=pointdata,aes(x=dist2river,y=woody))+
               se=T) 
 p2
 
-# Model_burnfreq: burning frequency predicted by Core Protected Areas and Rainfall
+p3<-ggplot(data=pointdata,aes(x=rainfall,y=woody))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p3
+
+
+p4<-ggplot(data=pointdata,aes(x=elevation,y=woody))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p4
+
+# Hills is yes or no > glm
+p5<-ggplot(data=pointdata,aes(y=woody,x=hills))+
+  geom_jitter(width = 0.05, height = 0.1) +
+  geom_smooth(method="glm",
+              method.args=list(family=quasipoisson),  # close to glm.nb
+              formula= y~x,
+              se=T)
+p5
+
+
+# Model_burnfreq: burning frequency predicted by dist2river and rainfall
 model_burnfreq_init <- glm(burnfreq ~ dist2river + rainfall, 
               family=poisson, 
               data = pointdata)
@@ -53,79 +76,85 @@ dispersion_stat
 # If 𝜙>1 : Overdispersion is present → Consider quasi-Poisson or negative binomial.
 # If 𝜙<1 : Underdispersion (less common) → Investigate the data further.
 
-### there is overdispersion
+### there is overdispersion so use a quasi-Poisson
 
 library(MASS)
 model_burnfreq <- MASS::glm.nb(burnfreq ~ dist2river + rainfall, 
               data = pointdata)
 summary(model_burnfreq)
 
-p3<-ggplot(data=pointdata,aes(y=burnfreq,x=dist2river))+
+p6<-ggplot(data=pointdata,aes(y=burnfreq,x=dist2river))+
   geom_jitter(width = 0.05, height = 0.1) +
   geom_smooth(method="glm",
               method.args=list(family=quasipoisson),  # close to glm.nb
               formula= y~x,
               se=T)
-p3
-p4<-ggplot(data=pointdata,aes(y=burnfreq,x=rainfall))+
-  geom_jitter(width = 0.05, height = 0.1) +
-  geom_smooth(method="glm",
-              method.args=list(family=quasipoisson),
-              formula= y~x,
-              se=T)
-p4
-
-# model_dist2river:  predicted by elevation
-model_dist2river <-glm(dist2river~rainfall + elevation,
-                      family=quasipoisson,
-                      data=pointdata)
-summary(model_dist2river)
-p5<-ggplot(data=pointdata,aes(y=dist2river,x=elevation))+
-  geom_jitter(height = 0.02) +
-  geom_smooth(method="glm",
-              method.args=list(family=quasipoisson),
-              formula= y~x,
-              se=T)
-p5
-
-p6<-ggplot(data=pointdata,aes(y=dist2river,x=rainfall))+
-  geom_jitter(width = 0.05, height = 0.1) +
-  geom_smooth(method="glm",
-              method.args=list(family=quasipoisson),
-              formula= y~x,
-              se=T)
 p6
 
-# model_rainfall: rainfall predicted by elevation
-model_rainfall <- glm(rainfall ~ elevation+hills, 
-              data = pointdata)
-summary(model_rainfall)
-
-p7<-ggplot(data=pointdata,aes(y=rainfall,x=elevation))+
-  geom_point() +
-  geom_smooth(method="lm",
+p7<-ggplot(data=pointdata,aes(y=burnfreq,x=rainfall))+
+  geom_jitter(width = 0.05, height = 0.1) +
+  geom_smooth(method="glm",
+              method.args=list(family=quasipoisson),
               formula= y~x,
               se=T)
 p7
 
-p8<-ggplot(data=pointdata,aes(y=rainfall,x=hills))+
+# model_dist2river: predicted by rainfall and elevation and hills
+model_dist2river <- lm(dist2river ~  rainfall + elevation + hills, 
+                  data = pointdata)
+summary(model_dist2river)
+p8<-ggplot(data=pointdata,aes(x=rainfall,y=dist2river))+
   geom_point() +
   geom_smooth(method="lm",
               formula= y~x,
               se=T)
 p8
 
+
+p9<-ggplot(data=pointdata,aes(x=elevation,y=dist2river))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p9
+
+p10<-ggplot(data=pointdata,aes(x=hills,y=dist2river))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p10
+
+# model_rainfall: rainfall predicted by elevation and hills
+model_rainfall <- lm(rainfall ~  elevation + hills, 
+                  data = pointdata)
+summary(model_rainfall)
+p11<-ggplot(data=pointdata,aes(x=elevation,y=rainfall))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p11
+
+p12<-ggplot(data=pointdata,aes(x=hills,y=rainfall))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula= y~x,
+              se=T)
+p12
+
 # combine the figures
 library(patchwork)
-allplots<-p1+p2+p3+p4+p5+p6+p7+p8+
-  patchwork::plot_layout(ncol=3) +
+allplots<-p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12+
+  patchwork::plot_layout(ncol=4) +
   patchwork::plot_annotation(title="Relations in the model")
 allplots
 
 ####### Combine all models into a single piecewise SEM
 psem_model <- piecewiseSEM::psem(model_woody,
                                  model_burnfreq,
-                                 model_rainfall)
+                                 model_rainfall,
+                                 model_dist2river)
 
 # Summarize the SEM results
 summary(psem_model)
